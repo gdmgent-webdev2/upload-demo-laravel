b23e2c6fa067ff34a74159d1b0f0ff5518426a2d
Frederick Roegiers <frederick@rogerthat.be>
2022-04-19 15:18:18 +0200

    wip

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
     public function show() {
+
+        Storage::disk('public')->put('example.txt', 'Hello NMD2');
+
         return view('upload');
     }
  
